05f00b3ae3f0b4d5613dcc6ce34043c2232faecb
muhqutsah <muhqutsah@gmail.com>
2025-07-04 12:28:25 +0700

    Menambahkan fitur GUI melalui website

--- main.py
+++ main.py
@@ -1,1 +1,41 @@
-import flask
+print("hello World!!")
+
+from flask import Flask, render_template, request
+from flask_cors import CORS
+from news_generator import make_news
+import threading
+import socket
+import time
+import webbrowser
+
+app = Flask(__name__)
+CORS(app)
+
+news_url_list = []
+
+def is_port_used(port):
+   with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+      return s.connect_ex(("localhost", port)) != 0
+
+def on_ready():
+  while is_port_used(5000):
+     time.sleep(0.1)
+  webbrowser.open_new("http://localhost:5000")
+
+@app.route("/")
+def main():
+    return render_template("index.html")
+
+@app.route("/generate", methods=["POST"])
+def generate():
+  if request.method == "POST":
+    make_news(request.json["URLList"])
+  return "News generated"
+
+def main():
+  threading.Thread(target=on_ready).start()
+  if(not is_port_used(5000)):
+    app.run("127.0.0.1", 5000)
+
+if __name__ == "__main__":
+   main()
--- news_generator.py
+++ news_generator.py
@@ -21,7 +21,7 @@ headers_requests = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:
 
 news_structure = {"src": "", "page": {"width": 8.27, "height": 11.69, "mtop": 0.5, "mbot": 0.5, "mlef": 0.5, "mrig": 0.5}, "title": {"font": "Times New Roman", "size": 22, "bold": True, "content": ""}, "quotes": {"font": "Times New Roman", "size": 10, "italic": True, "content": ""}, "image": {"height": 2.36, "src": ""}, "paragraphs": [{"font": "Times New Roman", "size": 11, "content": ""}]}
 
-system_prompt = f"Buat berita yang di kirimkan oleh user menjadi berita yang menarik dan bermodel paragraf, gunakan format json di setiap style font, gunakan struktur seperti berikut {str(news_structure)}, pastikan isi berita dapat pas pada ukuran kertas yang diberikan (Inch), ukuran font (pt), gunakan bahasa Indonesia, jika berita yang diberikan oleh user kosong maka tuliskan gagal di setiap content, berikan dateline di awal berita yang mengandung tempat dan sumber berita, jangan berikan tulisan copyright, buat minimal 5 paragraf, buat masing masing paragraf berisi 5 baris, gunakan gaya bahasa seperti artikel ilmiah populer"
+system_prompt = f"Buat berita yang di kirimkan oleh user menjadi berita yang menarik dan bermodel paragraf, gunakan format json di setiap style font, gunakan struktur seperti berikut {str(news_structure)}, pastikan isi berita dapat pas pada ukuran kertas yang diberikan (Inch), ukuran font (pt), gunakan bahasa Indonesia, jika berita yang diberikan oleh user kosong maka tuliskan gagal di setiap content, berikan dateline di awal berita yang mengandung tempat dan sumber berita, jangan berikan tulisan copyright, buat 6 paragraf, buat masing masing paragraf berisi 5 baris, gunakan gaya bahasa seperti artikel ilmiah populer"
 
 def generate_news(news):
   for i in range(3):
@@ -31,7 +31,6 @@ def generate_news(news):
         {"role": "user", "content": news}
       ], model="deepseek-r1-distill-llama-70b", reasoning_format="parsed", response_format={"type": "json_object"}, stream=False)
       result = generated_news.choices[0].message.content
-      print("News has generated")
       return json.loads(result)
     except:
       print(f"News failed to generate. Attemps: {i+1}")
@@ -77,14 +76,10 @@ def get_news(url):
         time.sleep(0.5)
       else:
         break
-    except:
-      print()
+    except Exception as e:
+      print(e)
       print(f"Getting news failed. Attemp: {i+1}")
       time.sleep(0.5)
-  if res_arr == []:
-    print("Getting news failed")
-  else:
-    print("News has gotten")
   return {"news": " ".join(res_arr), "img": image}
 
 def create_page(data, img, news_length, news_index):
@@ -144,10 +139,9 @@ def create_document(data):
   
   document.save(f"sans {edition}.docx")
 
-def news_process(url_arr):
+def make_news(url_arr):
   news_result = []
   for index, url in enumerate(url_arr):
-    news_result = []
     scrap_result = get_news(url)
     news = generate_news(scrap_result["news"])
     if scrap_result["img"] is not None:
@@ -156,9 +150,7 @@ def news_process(url_arr):
       news["image"]["src"] = "placeholder"
     news["src"] = urlparse(url).netloc
     news_result.append(news)
-    for data in news_result:
-      print(data["title"]["content"])
-      create_page(data=data, img=data["image"], news_length=len(url_arr), news_index=index)
-    print(f"News ({index+1}/{len(url_arr)}) created")
+    create_page(data=news, img=news["image"], news_length=len(url_arr), news_index=index)
+    print(f"News ({index+1}/{len(url_arr)}) created: {news["title"]["content"]}")
     time.sleep(0.3)
-  create_document(data)
+  create_document(news_result[0])
